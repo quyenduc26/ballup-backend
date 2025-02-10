9ea4f12b5ca65cb72d07c7340e70260a6e65a9fe
quyenduc26 <quyenduc26@gmail.com>
2025-02-10 08:52:57 +0700

    initial src

---
+++ src/main/java/com/example/ballup_backend/BallupBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ballup_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BallupBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BallupBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ballup_backend/BallupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ballup_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BallupBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
