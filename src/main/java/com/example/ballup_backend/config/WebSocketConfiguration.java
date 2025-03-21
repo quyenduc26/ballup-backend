110ef57b2f7bd9e404f840aed43b47926abc250b
quyenduc26 <quyenduc26@gmail.com>
2025-03-21 21:31:47 +0700

    change ws origin

--- src/main/java/com/example/ballup_backend/config/WebSocketConfiguration.java
+++ src/main/java/com/example/ballup_backend/config/WebSocketConfiguration.java
@@ -20,7 +20,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-        .setAllowedOrigins("http://localhost:3000/")
+        .setAllowedOrigins("https://ballup-backend.onrender.com")
         .withSockJS();
     }
 
