0f5f7892093396cfc6324584decc21436a58ccb6
quyenduc26 <quyenduc26@gmail.com>
2025-03-24 11:11:49 +0700

    comment env

--- src/main/java/com/example/ballup_backend/BallupBackendApplication.java
+++ src/main/java/com/example/ballup_backend/BallupBackendApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import io.github.cdimascio.dotenv.Dotenv;
+// import io.github.cdimascio.dotenv.Dotenv;
 
 
 @SpringBootApplication
@@ -14,8 +14,8 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class BallupBackendApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		// Dotenv dotenv = Dotenv.load();
+        // dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(BallupBackendApplication.class, args);
 		
